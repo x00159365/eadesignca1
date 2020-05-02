0580fd987c8cae661a3e4339a2eb125b20a702be
Michael Guiney <X00159365@computing.stu.it-tallaght.ie>
2020-05-02 11:03:55 +0100

    update sec_con.go so that msg content is used.

--- async/seccon/go/sec_con.go
+++ async/seccon/go/sec_con.go
@@ -1,23 +1,24 @@
 package seccon
 
 import (
-	"net/http"
 	"html/template"
+	"net/http"
 	"time"
-	"strconv"
 )
 
-var counts map[string]int = make(map[string]int)
+var counts map[string]string = make(map[string]string)
 var channelNames []string
 
 func ProcessMessage(channel_name string, msg_content string) {
 	// the function just counts the messages so message_content goes unused
-	counts[channel_name]++
+	//counts[channel_name]++
+	// MG changing to use the content
+	counts[channel_name] = msg_content
 }
 
 func SetUpSecConPubSub(redis_url string, interval time.Duration, wait time.Duration, channel_names []string) {
 
-	channelNames = channel_names;
+	channelNames = channel_names
 	SetUpPubSub("security-console", redis_url, interval, wait, channel_names, ProcessMessage)
 }
 
@@ -28,7 +29,9 @@ func GetTableInnerHTML() string {
 	}
 	innerHTML += "</tr><tr>"
 	for _, name := range channelNames {
-		innerHTML += "<th>" + strconv.Itoa(counts[name]) + "</th>"
+		// MG
+		//innerHTML += "<th>" + strconv.Itoa(counts[name]) + "</th>"
+		innerHTML += "<th>" + counts[name] + "</th>"
 	}
 	innerHTML += "</tr>"
 	return innerHTML
@@ -38,11 +41,11 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	inserts := struct {
-    		TableInnerHTML template.HTML	
+		TableInnerHTML template.HTML
 	}{
 		template.HTML(GetTableInnerHTML()),
 	}
 	t, _ := template.ParseFiles("console.html")
-	
+
 	t.Execute(w, inserts)
 }
